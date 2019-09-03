73696dbb9a4be7ce590c247c3054fb5a595053be
nicholasdwebb <nicholasdwebb@me.com>
2019-09-02 22:06:52 -0400

    Fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(function(){
 	function loadImages(col, i){
+		console.log(i);
 		$("#gal-col-"+col).append('<img id="gal-img-'+i+'" src="Pics/Gallery/'+i+'.jpg" onerror="$(this).remove();" />'
 			+'<img id="gal-img-'+i+'" src="Pics/Gallery/'+i+'.png" onerror="$(this).remove();" />'
-			+'<img id="gal-img-'+i+'" src="Pics/Gallery/'+i+'.PNG" onerror="$(this).remove();" />')
-			.ready(setTimeout(function(){
-				if($('#gal-img-'+i).length != 0){
-					// console.log(i);
-					i++
-					col++
-					if(col == 4){
-						col = 1;
-					}
-					loadImages(col, i);
-				}
-			},25));
+			+'<img id="gal-img-'+i+'" src="Pics/Gallery/'+i+'.PNG" onerror="$(this).remove();" />');
+		if(i<$("#picCount").html()){
+			i++
+			col++
+			if(col == 4){
+				col = 1;
+			}
+			loadImages(col, i);
+		}else{
+			$("#picCount").remove();
+		}
 	}
 	loadImages(1,1);
-})
+});
