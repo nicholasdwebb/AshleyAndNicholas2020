bcfac07b5dbf540278660bcea1355df107bc215f
nicholasdwebb <nicholasdwebb@me.com>
2019-09-03 21:59:40 -0400

    improved mouse/touch events

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ $(function(){
 	setTimeout(function f(){
 		if(windowHeight != window.innerHeight){
 			windowHeight = window.innerHeight;
-			$("#mainWindow").height(windowHeight - $("nav").height());
-			console.log(windowHeight);
+			$("#mainWindow").height(windowHeight - $("nav").height() - 16);
+			// console.log(windowHeight);
 		}
 		setTimeout(f,250);
 	}, 250);
@@ -21,4 +21,9 @@ $(function(){
 			$("#mainWindow").animate({opacity: 1}, 150);
 		},150);
 	}
+	$("#mainWindow").mouseenter(function(){
+		if(window.innerWidth <= 768 && $("nav").height() >= 50){
+			$(".navbar-toggler-icon").click();
+		}
+	});
 });
