1945cce94ffda226c5660f83bf87f812f9f78512
nicholasdwebb <nicholasdwebb@me.com>
2019-09-05 16:55:00 -0400

    added trash panda

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(function(){
+	//general
 	var windowHeight = 0;
 	setTimeout(function f(){
 		if(windowHeight != window.innerHeight){
@@ -8,16 +9,19 @@ $(function(){
 		}
 		setTimeout(f,250);
 	}, 250);
+	swapMainWindow("#aboutUsWrapper");
 	$("#aboutUsNavbar").click(function(){
-		swapIframe("aboutus.html");
+		swapMainWindow("#aboutUsWrapper");
 	});
 	$("#galleryNavbar").click(function(){
-		swapIframe("gallery.html");
+		swapMainWindow("#galleryWrapper");
+		setTimeout(startGallery,500);
 	});
-	function swapIframe(str){
+	function swapMainWindow(str){
 		$("#mainWindow").animate({opacity: 0}, 150);
 		setTimeout(function(){
-			$("#mainWindow").attr("src", str);
+			$("#mainWindow").html($(str).html());
+			$(window).scrollTop(0);
 			$("#mainWindow").animate({opacity: 1}, 150);
 		},150);
 	}
@@ -26,4 +30,36 @@ $(function(){
 			$(".navbar-toggler-icon").click();
 		}
 	});
+
+	//gallery
+	function startGallery(){
+		var galIndex = 0;
+		$("#gallery img").each(function(i,e){
+			// console.log(i,e);
+			$(e).click(function(){
+				$("#viewer").css("display","flex");
+				$("#viewer img").attr("src",$(e).attr("src"));
+				galIndex = i;
+			});
+		});
+		$("#viewer-background, #viewer img").click(function(){
+			$("#viewer").hide();
+		});
+		$("#prev").click(function(){
+			$("#viewer").show();
+			galIndex--;
+			if(galIndex < 0){
+				galIndex = $("#gallery img").length - 1;
+			}
+			$("#viewer img").attr("src",$("#gallery img:eq("+galIndex+")").attr("src"));
+		});
+		$("#next").click(function(){
+			$("#viewer").show();
+			galIndex++;
+			if(galIndex >= $("#gallery img").length){
+				galIndex = 0;
+			}
+			$("#viewer img").attr("src",$("#gallery img:eq("+galIndex+")").attr("src"));
+		});
+	}
 });
